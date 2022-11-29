11ed2a67b3d2b3f16eacd06de25197fb49f66397
adhith-c <adhithadhi17@gmail.com>
2022-11-29 10:24:55 +0530

    Rowpost added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Banner from './Banner/Banner';
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Banner/>
     </div>
   );
 }
